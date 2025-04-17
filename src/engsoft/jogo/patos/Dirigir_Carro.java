6ea95001fe4ee3d7c56191b9f7573376b2841457
Patrick Augusto <79328506+Patrick-Augusto@users.noreply.github.com>
2025-04-16 22:35:13 -0300

    ADD Comentarios

--- src/engsoft/jogo/patos/Dirigir_Carro.java
+++ src/engsoft/jogo/patos/Dirigir_Carro.java
@@ -1,5 +1,16 @@
 package engsoft.jogo.patos;
 
+/**
+ * Classe Dirigir_Carro
+ * 
+ * Esta classe implementa a interface Padrao_Dirigir como parte do padrão
+ * Strategy.
+ * Representa entidades que possuem a capacidade de dirigir carros.
+ * 
+ * Esta implementação concreta do algoritmo de direção retorna uma mensagem
+ * indicando que está dirigindo um carro específico.
+ */
+
 public class Dirigir_Carro implements Padrao_Dirigir {
 
     public Dirigir_Carro() {
@@ -9,5 +20,5 @@ public class Dirigir_Carro implements Padrao_Dirigir {
     @Override
     public String dirigir() {
         return "Dirigindo um corsa... 🚘";
-    }    
+    }
 }
